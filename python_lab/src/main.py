ba2f3c79b75c1a448576dfb54e0424b36593859a
stephane dieumerci <dieu8364@amalaeducation.org>
2026-08-20 10:18:15 +0000

    debuging

--- python_lab/src/main.py
+++ python_lab/src/main.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 
-from utils.py import *
+from utils import *
 
 def main():
-   try:
+
       num = int(input("enter a number pls: "))
 
       sq_val = square(num)
       even_odd_val = is_even(num)
       f_val = celsius_to_fahrenheit(num)
 
+      even_odd_str = "even" if even_odd_val else "odd"
+
 
       print(f"Square: {sq_val}")
       print(f"Parity: {even_odd_val}")
+      print(f"even or odd: {even_odd_str} ")
       print(f"Fahrenheit equivalent: {f_val}°F")
 
-   except ValueError:
-        print("Please enter a valid numeric value.")
 
+if __name__=="__main__":
+      main()
